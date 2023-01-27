f7dc59a3f6da97b0b22ee3d3a4010d24c20ff18f
palpablejc <77988503+palpablejc@users.noreply.github.com>
2023-01-27 15:57:26 -0600

    trying to use google login

--- WebAppSandbox React/training-app/src/App.js
+++ WebAppSandbox React/training-app/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
+// import { useGoogleLogout } from 'react-google-login';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+// const clientId = '244658728792-9jpa8bht2tprd3rkoo1gh7jo2uf97fhf.apps.googleusercontent.com';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedInWithGoogle, setIsLoggedInWithGoogle] = useState(false);
 
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
@@ -13,27 +17,49 @@ function App() {
     if (storedUserLoggedInInformation === '1') {
       setIsLoggedIn(true);
     }
-
   }, []);
 
-  const loginHandler = (email, password) => {
+  // const { signOut } = useGoogleLogout({
+  //   clientId,
+  //   onLogoutSuccess,
+  //   onFailure
+  // });
+
+  // const onLogoutSuccess = () => {
+  //   console.log('google logout successful');
+  // };
+
+  // const onFailure = () => {
+  //   console.log('failed to logout of google');
+  // };
+
+  const loginHandler = (loginParams) => {
     // TODO: Check email or password
-    // Need to introduce google one tap
-    
+
     localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
+
+    if (loginParams.IsLoggedInWithGoogle) {
+      setIsLoggedInWithGoogle(true);
+    }
   };
 
   const logoutHandler = () => {
+    if (isLoggedInWithGoogle) {
+      setIsLoggedInWithGoogle(false);
+      // signOut();
+    } else {
+      setIsLoggedIn(false);
+    }
+
     localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
   };
 
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {!isLoggedIn && <Login onLogin={loginHandler} isLoggedIn={isLoggedIn}/>}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
     </React.Fragment>
--- WebAppSandbox React/training-app/src/components/Login/Login.js
+++ WebAppSandbox React/training-app/src/components/Login/Login.js
@@ -3,7 +3,13 @@ import React, { useState, useEffect } from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
-import GoogleLogin from './GoogleLogin';
+
+import GoogleLogin from 'react-google-login';
+
+// import { useGoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+const clientId = '244658728792-9jpa8bht2tprd3rkoo1gh7jo2uf97fhf.apps.googleusercontent.com'; // TODO: Read this from a keyvault/secret (?)
 
 const Login = (props) => {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -13,13 +19,45 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
   }, [enteredEmail, enteredPassword]);
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: '',
+      });
+    };
+    gapi.load('client:auth2', initClient);
+  }), [props.isLoggedIn];
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
+  };
+
+  // const { signin } = useGoogleLogin({
+  //   onSuccess,
+  //   onFailure,
+  //   clientId,
+  //   isSignedIn: true
+  // })
+
+  const onSuccess = (res) => {
+    console.log('success:', res);
+    setEnteredEmail(res.profileObj.email);
+
+    const arg = {
+      email: enteredEmail,
+      password: '',
+      isLoggedInWithGoogle: true
+    }
+
+    props.onLogin(arg);
+  };
+
+  const onFailure = (err) => {
+    console.log('failed:', err);
   };
 
   const passwordChangeHandler = (event) => {
@@ -35,6 +73,7 @@ const Login = (props) => {
   };
 
   const submitHandler = (event) => {
+    console.log(event);
     event.preventDefault();
     props.onLogin(enteredEmail, enteredPassword);
   };
@@ -42,40 +81,46 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
+        <div className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''}`}>
+          <label htmlFor='email'>E-Mail</label>
           <input
-            type="email"
-            id="email"
+            type='email'
+            id='email'
             value={enteredEmail}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
+        <div className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''}`}>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             value={enteredPassword}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
         </div>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type='submit' className={classes.btn} disabled={!formIsValid}>
             Login
           </Button>
-
-          <GoogleLogin />
+        </div>
+        <br />
+        {/* <div className={classes.actions}>
+          <Button onClick={signin} type='submit' className={classes.btn} disabled={!formIsValid}>
+             Login with Google
+          </Button>
+        </div> */}
+        <div className={classes.actions}>
+          <GoogleLogin
+            clientId={clientId}
+            buttonText='Sign in with Google'
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={'single_host_origin'}
+            isSignedIn={true}
+          />
         </div>
       </form>
     </Card>
